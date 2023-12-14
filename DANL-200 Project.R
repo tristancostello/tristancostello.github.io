0c769253c86f726217c1faa6a67893d5139debd5
tristancostello <tc32@geneseo.edu>
2023-12-14 14:48:02 -0500

    nba ny

--- DANL-200 Project.R
+++ DANL-200 Project.R
@@ -1,1 +1,11 @@
 install.packages("hoopR")
+  library(hoopR)
+if (!requireNamespace('pacman', quietly = TRUE)){
+  install.packages('pacman')
+}
+pacman::p_load_current_gh("sportsdataverse/hoopR", dependencies = TRUE, update = TRUE)
+
+progressr::with_progress({
+  nba_pbp <- hoopR::load_nba_pbp()
+})
+tictoc::toc()
---
+++ Untitled.R
@@ -0,0 +1,10 @@
+install.packages("usethis")
+usethis::create_github_token()
+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
+install.packages("gitcreds")
+gitcreds::gitcreds_set()
+
+git add .
+git commit -m “ANY_MASSAGE”
+git push -u origin main
+install.packages("hrbrthemes")
